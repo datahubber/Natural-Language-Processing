99bf07c2886df111c433cea12c1e3396c59e3a8e
Albert Wang <123819248+datahubber@users.noreply.github.com>
2025-08-01 17:18:54 -0700

    Delete src directory

--- src/__init__.py
+++
@@ -1,9 +0,0 @@
-# Mechanistic Interpretability Assessment Package
-
-from .activations import *
-from .sae import *
-from .feature_extraction import *
-from .utils import *
-
-__version__ = "1.0.0"
-__author__ = "Assessment Student" 
